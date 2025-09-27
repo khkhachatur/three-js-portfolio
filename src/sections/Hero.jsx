549bb824a156976ac8f74d9a119d98265bc1e6fa
Khachatryan Khachatur <khachatryankhachatur57@gmail.com.com>
2025-09-27 15:27:52 +0100

    hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,11 +24,10 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am Adrian <span className="waving-hand">👋</span>
-        </p>
-        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space pb-30px">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">Hi I am Khachatur</p>
+        <img className="car-simulation" src="/assets/f1.png" alt="/"></img>
+        <p className="hero_tag text-gray_gradient">Frontend Developer & GIS PM</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
