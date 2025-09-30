cacd86adc4b1f0dc1eda735a10a05a480ff72946
Khachatryan Khachatur <khachatryankhachatur57@gmail.com.com>
2025-09-30 11:50:50 +0100

    vite js update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext', // allows top-level await
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext', // ensures deps also compile with ESNext
+    },
+  },
+});
