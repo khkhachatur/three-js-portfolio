dab865ccca2962df5b9a243af9ba145601882899
Khachatryan Khachatur <khachatryankhachatur57@gmail.com.com>
2025-10-01 18:37:53 +0100

    experience section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,5 +1,32 @@
+import { workExperiences } from '../constants';
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section className="my-20 c-space">
+      <div className="work-content">
+        <div className="sm:py-10 py-5 sm:px-5 px-2.5">
+          {workExperiences.map(({ id, name, icon, pos, duration, title }) => (
+            <div key={id} className="work-content">
+              <div className="text-white-600 w-full">
+                <div className="work-content_container ">
+                  <div className="work-content_logo">
+                    <img src={icon} alt="logo" className="w-full h-full" />
+                  </div>
+                  <div className="sm:py-5 p-2.5 sm:px-2.5 1.5">
+                    <p className="text-white-700 font-bold">{name}</p>
+                    <p className="text-white-500 font-sm">
+                      {pos} -- {duration}
+                    </p>
+                    <p className="group-hover:text-white transition-all ease-in-out duration-500">{title}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,31 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="c-space pt-7 pb-3 flex justify-between items-center flex-wrap gap-5 border-t border-black-300">
+      <div className="text-white-500 flex gap-3">
+        <p>Terms and Conditions</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+      </div>
+      <div className="flex gap-5">
+        <div className="social-icon">
+          <a href="https://www.instagram.com/khcho_k/" target="_blank" rel="noopener noreferrer">
+            <img src="assets/instagram.png" alt="instagram-icon" className="w-7 h-7" />
+          </a>
+        </div>
+        <div className="social-icon">
+          <a href="https://web.telegram.org/#@kkkk_kkkk_kkkk_k" target="_blank" rel="noopener noreferrer">
+            <img src="assets/telegram.png" alt="instagram-icon" className="w-7 h-7" />
+          </a>
+        </div>
+        <div className="social-icon">
+          <a href="https://github.com/khkhachatur" className="hover:pointer" target="_blank" rel="noopener noreferrer">
+            <img src="assets/github.png" alt="instagram-icon" className="w-7 h-7" />
+          </a>
+        </div>
+      </div>
+      <p className="text-white-500">© 2025 K.Khachatur . All rights reserved.</p>
+    </section>
+  );
 };
 
 export default Footer;
